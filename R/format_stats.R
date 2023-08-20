b50e6d34e4018bd53f3f166fb40a85c5bbfd335f
Jeffrey R. Stevens <jeffrey.r.stevens@protonmail.com>
2023-08-20 14:14:22 -0500

    Switch to case_when() and add argument assertion tests

--- R/format_stats.R
+++ R/format_stats.R
@@ -1,37 +1,35 @@
 #' Format correlation statistics
 #'
 #' @param x Correlation object
-#' @param digits Number of digits after the decimal for means, correlation
-#' coefficient, confidence intervals, and degrees of freedom
-#' @param pdigits Number of digits after the decimal for p-values (also controls
-#' cutoff for small p-values)
+#' @param digits Number of digits after the decimal for means, confidence
+#' intervals, and correlation coefficients
+#' @param pdigits Number of digits after the decimal for p-values, ranging
+#' between 1-5 (also controls cutoff for small p-values)
 #' @param pzero Logical indicator of whether to include leading zero for
 #' p-values
 #' @param ci Logical indicator of whether to print 95% confidence intervals
-#' @param italics Logical for whether _p_ label should be italicized or not
+#' @param italics Logical for whether _p_ label should be italicized
 #' @param dfs Formatting for degrees of freedom ("par" = parenthetical,
 #' "sub" = subscript, "none" = do not print degrees of freedom)
 #' @param type Type of formatting ("md" = markdown, "latex" = LaTeX)
 #'
 #' @return
-#' A character string of statistical information formatted in Markdown.
+#' A character string of statistical information formatted in Markdown or LaTeX.
 #' @export
 #'
 #' @family functions for printing statistics
 #'
 #' @examples
 #' # Prepare data
-#' beavers <- merge(beaver1, beaver2, by = "time")
-#' beavers <- beavers[, c("time", "temp.x", "temp.y")]
-#' beavers_corr <- cor.test(beavers$temp.x, beavers$temp.y)
+#' mtcars_corr <- cor.test(mtcars$mpg, mtcars$disp)
 #' # Print statistics
-#' format_corr(beavers_corr)
+#' format_corr(mtcars_corr)
 #' # Change digits
-#' format_corr(beavers_corr, digits = 3)
+#' format_corr(mtcars_corr, digits = 3)
 #' # Change cutoff digits for p-value
-#' format_corr(beavers_corr, pdigits = 2)
+#' format_corr(mtcars_corr, pdigits = 2)
 #' # Add leading zero to p-value and don't print confidence intervals
-#' format_corr(beavers_corr, pzero = TRUE, ci = FALSE)
+#' format_corr(mtcars_corr, pzero = TRUE, ci = FALSE)
 
 format_corr <- function(x,
                         digits = 2,
@@ -41,41 +39,41 @@ format_corr <- function(x,
                         italics = TRUE,
                         dfs = "par",
                         type = "md") {
+  # Check arguments
+  stopifnot("Input must be a correlation object." = inherits(x, what = "htest"))
+  stopifnot("Argument `digits` must be a non-negative numeric vector." = is.numeric(digits))
+  stopifnot("Argument `digits` must be a non-negative numeric vector." = digits >= 0)
+  stopifnot("Argument `pdigits` must be a numeric between 1 and 5." = is.numeric(pdigits))
+  stopifnot("Argument `pdigits` must be a numeric between 1 and 5." = pdigits > 0)
+  stopifnot("Argument `pdigits` must be a numeric between 1 and 5." = pdigits < 6)
+  stopifnot("Argument `pzero` must be TRUE or FALSE." = is.logical(pzero))
+  stopifnot("Argument `ci` must be TRUE or FALSE." = is.logical(ci))
+  stopifnot("Argument `italics` must be TRUE or FALSE." = is.logical(italics))
+  stopifnot("Argument `dfs` must be 'par', 'sub', or 'none'." = dfs %in% c("par", "sub", "none"))
+  stopifnot("Argument `type` must be 'md' or 'latex'." = type %in% c("md", "latex"))
+
   # Format numbers
   corr <- format_num(x$estimate, digits = digits)
   cis <- format_num(x$conf.int, digits = digits)
-  df <- format_num(x$parameter, digits = digits)
+  df <- dplyr::case_when(round(x$parameter, 1) == round(x$parameter) ~ format_num(x$parameter, digits = 0),
+                         .default = format_num(x$parameter, digits = digits))
   pvalue <- format_p(x$p.value, pdigits = pdigits, pzero = pzero,
                      italics = italics, type = type)
 
   # Build label
-  if (identical(type, "md") & italics) {
-    r_lab <- paste0("_r_")
-  } else if (identical(type, "latex") & italics) {
-    r_lab <- paste0("$r$")
-  } else if (!italics) {
-    r_lab <- paste0("r")
-  } else {
-    stop("Wrong type specified.")
-  }
-  if (identical(dfs, "par")) {
-    rlab <- paste0(r_lab, "(", df, ")")
-  } else if (identical(dfs, "sub") & identical(type, "md")) {
-    rlab <- paste0(r_lab, "~", df, "~")
-  } else if (identical(dfs, "sub") & identical(type, "latex")) {
-    rlab <- paste0(r_lab, "$_{", df, "}$")
-  } else if (identical(dfs, "none")) {
-    rlab <- r_lab
-  } else {
-    stop("Wrong dfs type specified.")
-  }
+  r_lab <- dplyr::case_when(!italics ~ paste0("r"),
+                            identical(type, "md") ~ paste0("_r_"),
+                            identical(type, "latex") ~ paste0("$r$"),
+  )
+  rlab <- dplyr::case_when(identical(dfs, "par") ~ paste0(r_lab, "(", df, ")"),
+                           identical(dfs, "sub") & identical(type, "md") ~ paste0(r_lab, "~", df, "~"),
+                           identical(dfs, "sub") & identical(type, "latex") ~ paste0(r_lab, "$_{", df, "}$"),
+                           identical(dfs, "none") ~ r_lab)
 
   # Create statistics string
-  if (ci) {
-    paste0(rlab, " = ", corr, ", 95% CI [", cis[1], ", ", cis[2], "], ", pvalue)
-  } else {
-    paste0(rlab, " = ", corr, ", ", pvalue)
-  }
+  full_lab <- dplyr::case_when(ci ~ paste0(rlab, " = ", corr, ", 95% CI [", cis[1], ", ", cis[2], "], ", pvalue),
+                               !ci ~ paste0(rlab, " = ", corr, ", ", pvalue))
+  return(full_lab)
 }
 
 
@@ -83,38 +81,36 @@ format_corr <- function(x,
 #'
 #' @param x t-test object
 #' @param digits Number of digits after the decimal for means, confidence
-#' intervals, t-statistic, and degrees of freedom
-#' @param pdigits Number of digits after the decimal for p-values (also controls
-#' cutoff for small p-values)
+#' intervals, and t-statistics
+#' @param pdigits Number of digits after the decimal for p-values, ranging
+#' between 1-5 (also controls cutoff for small p-values)
 #' @param pzero Logical indicator of whether to include leading zero for
 #' p-values
 #' @param full Logical indicator of whether to include means and confidence
 #' intervals or just t-statistic and p-value
-#' @param italics Logical for whether _p_ label should be italicized or not
+#' @param italics Logical for whether _p_ label should be italicized
 #' @param dfs Formatting for degrees of freedom ("par" = parenthetical,
 #' "sub" = subscript, "none" = do not print degrees of freedom)
 #' @param mean Formatting for mean label ("abbr" = M, "word" = Mean)
 #' @param type Type of formatting ("md" = markdown, "latex" = LaTeX)
 #'
 #' @return
-#' A character string of statistical information formatted in Markdown.
+#' A character string of statistical information formatted in Markdown or LaTeX.
 #' @export
 #'
 #' @family functions for printing statistics
 #'
 #' @examples
 #' # Prepare data
-#' beavers <- merge(beaver1, beaver2, by = "time")
-#' beavers <- beavers[, c("time", "temp.x", "temp.y")]
-#' beavers_tt <- t.test(beavers$temp.x, beavers$temp.y)
+#' mtcars_tt <- t.test(formula = mtcars$mpg ~ mtcars$vs)
 #' # Print statistics
-#' format_ttest(beavers_tt)
+#' format_ttest(mtcars_tt)
 #' # Change digits
-#' format_ttest(beavers_tt, digits = 2)
+#' format_ttest(mtcars_tt, digits = 2)
 #' # Change cutoff digits for p-value
-#' format_ttest(beavers_tt, pdigits = 2)
+#' format_ttest(mtcars_tt, pdigits = 2)
 #' # Add leading zero to p-value and don't print confidence intervals
-#' format_ttest(beavers_tt, pzero = TRUE, full = FALSE)
+#' format_ttest(mtcars_tt, pzero = TRUE, full = FALSE)
 format_ttest <- function(x,
                          digits = 1,
                          pdigits = 3,
@@ -124,63 +120,50 @@ format_ttest <- function(x,
                          dfs = "par",
                          mean = "abbr",
                          type = "md") {
+  # Check arguments
+  stopifnot("Input must be a correlation object." = inherits(x, what = "htest"))
+  stopifnot("Argument `digits` must be a non-negative numeric vector." = is.numeric(digits))
+  stopifnot("Argument `digits` must be a non-negative numeric vector." = digits >= 0)
+  stopifnot("Argument `pdigits` must be a numeric between 1 and 5." = is.numeric(pdigits))
+  stopifnot("Argument `pdigits` must be a numeric between 1 and 5." = pdigits > 0)
+  stopifnot("Argument `pdigits` must be a numeric between 1 and 5." = pdigits < 6)
+  stopifnot("Argument `pzero` must be TRUE or FALSE." = is.logical(pzero))
+  stopifnot("Argument `full` must be TRUE or FALSE." = is.logical(full))
+  stopifnot("Argument `italics` must be TRUE or FALSE." = is.logical(italics))
+  stopifnot("Argument `dfs` must be 'par', 'sub', or 'none'." = dfs %in% c("par", "sub", "none"))
+  stopifnot("Argument `mean` must be 'abbr' or 'word'." = mean %in% c("abbr", "word"))
+  stopifnot("Argument `type` must be 'md' or 'latex'." = type %in% c("md", "latex"))
+
   # Format numbers
   if (length(x$estimate) == 2) {
     mean_val <- format_num(x$estimate[1] - x$estimate[2], digits = digits)
   } else if (length(x$estimate) == 1) {
     mean_val <- format_num(x$estimate, digits = digits)
-  } else {
-    stop("Too many values in the t-test estimate.")
   }
   cis <- format_num(x$conf.int, digits = digits)
-  if (round(x$parameter, 1) == round(x$parameter)) {
-    df <- format_num(x$parameter, digits = 0)
-  } else {
-    df <- format_num(x$parameter, digits = digits)
-  }
+  df <- dplyr::case_when(round(x$parameter, 1) == round(x$parameter) ~ format_num(x$parameter, digits = 0),
+                         .default = format_num(x$parameter, digits = digits))
   tstat <- format_num(x$statistic, digits = digits)
   pvalue <- format_p(x$p.value, pdigits = pdigits, pzero = pzero,
                      italics = italics, type = type)
 
   # Build label
-  if (identical(type, "md") & italics) {
-    t_lab <- paste0("_t_")
-  } else if (identical(type, "latex") & italics) {
-    t_lab <- paste0("$t$")
-  } else if (!italics) {
-    t_lab <- paste0("t")
-  } else {
-    stop("Wrong type specified. Should be 'md' or 'latex'.")
-  }
-  if (identical(dfs, "par")) {
-    tlab <- paste0(t_lab, "(", df, ")")
-  } else if (identical(dfs, "sub") & identical(type, "md")) {
-    tlab <- paste0(t_lab, "~", df, "~")
-  } else if (identical(dfs, "sub") & identical(type, "latex")) {
-    tlab <- paste0(t_lab, "$_{", df, "}$")
-  } else if (identical(dfs, "none")) {
-    tlab <- t_lab
-  } else {
-    stop("Wrong dfs type specified. Should be 'par', 'sub', or 'none'.")
-  }
+  t_lab <- dplyr::case_when(!italics ~ paste0("t"),
+                            identical(type, "md") ~ paste0("_t_"),
+                            identical(type, "latex") ~ paste0("$t$"))
+  tlab <- dplyr::case_when(identical(dfs, "par") ~ paste0(t_lab, "(", df, ")"),
+                           identical(dfs, "sub") & identical(type, "md") ~ paste0(t_lab, "~", df, "~"),
+                           identical(dfs, "sub") & identical(type, "latex") ~ paste0(t_lab, "$_{", df, "}$"),
+                           identical(dfs, "none") ~ t_lab)
 
   # Create statistics string
   if (full) {
-    if (identical(mean, "abbr") & italics & identical(type, "md")) {
-      mean_lab <- "_M_ = "
-    } else if (identical(mean, "abbr") & italics & identical(type, "latex")){
-      mean_lab <- "$M$ = "
-    } else if (identical(mean, "abbr") & !italics){
-      mean_lab <- "M = "
-    } else if (identical(mean, "word") & italics & identical(type, "md")){
-      mean_lab <- "_Mean_ = "
-    } else if (identical(mean, "word") & italics & identical(type, "latex")){
-      mean_lab <- "$Mean$ = "
-    } else if (identical(mean, "word") & !italics){
-      mean_lab <- "Mean = "
-    } else {
-      stop("Wrong mean type specified. Should be 'abbr' or 'word'.")
-    }
+    mean_lab <- dplyr::case_when(identical(mean, "abbr") & italics & identical(type, "md") ~ "_M_ = ",
+                                 identical(mean, "abbr") & italics & identical(type, "latex") ~ "$M$ = ",
+                                 identical(mean, "abbr") & !italics ~ "M = ",
+                                 identical(mean, "word") & italics & identical(type, "md") ~ "_Mean_ = ",
+                                 identical(mean, "word") & italics & identical(type, "latex") ~ "$Mean$ = ",
+                                 identical(mean, "word") & !italics ~ "Mean = ")
     paste0(mean_lab, mean_val, ", 95% CI [", cis[1], ", ", cis[2], "], ", tlab, " = ", tstat, ", ", pvalue)
   } else {
     paste0(tlab, " = ", tstat, ", ", pvalue)
